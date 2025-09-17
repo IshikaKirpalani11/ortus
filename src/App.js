07b41a012bdec26354ccabf2efcd43220130c37b
Ishika Kirpalani <ishikakirpalanii@gmail.com>
2025-09-18 00:57:55 +0500

    Add images and Netlify redirects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
+import Page3 from "./Page3";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/page3" element={<Page3 />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
